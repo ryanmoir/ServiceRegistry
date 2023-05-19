1c28a3030aa2ae591da6c0b5862010318c76490c
r <rm>
2023-05-19 16:36:47 +0100

    added repository for registory table

---
+++ ServiceRegistory/ServiceRegistory.EntityAcceess/Repository/Implmentation/RegistoryRespository.cs
@@ -0,0 +1,13 @@
+﻿using ServiceRegistory.Entity.Tables;
+using ServiceRegistory.EntityAcceess.Repository.Interface;
+using ServiceRegistory.EntityAcceess.UnitOfWork.Interface;
+
+namespace ServiceRegistory.EntityAcceess.Repository.Implmentation
+{
+    public class RegistoryRespository : GenericRepository<Registory>, IRegistoryRepository
+    {
+        public RegistoryRespository(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
